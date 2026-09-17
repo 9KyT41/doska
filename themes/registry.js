fc6407365ca4db224536fc907928799f07d89007
9KyT <-@.com>
2026-09-17 21:55:53 +1200

    темы: ватман в клетку стал дефолтом (первый в registry)

--- themes/registry.js
+++ themes/registry.js
@@ -1,4 +1,5 @@
 const THEMES = [
-  { id: 'oak',       name: 'Дуб (заглушка)',  file: 'themes/oak.css' },
-  { id: 'blueprint', name: 'Ватман в клетку', file: 'themes/blueprint.css' }
+  { id: 'blueprint', name: 'Ватман в клетку', file: 'themes/blueprint.css' },
+  { id: 'hud',       name: 'Неон (HUD)',      file: 'themes/hud.css' },
+  { id: 'oak',       name: 'Дуб (заглушка)',  file: 'themes/oak.css' }
 ];
